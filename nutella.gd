cb651eccee174135819934b195c99d486b58fd55
Insaly <81804810+Insaly@users.noreply.github.com>
2022-03-09 13:29:58 +0100

    Moviment Test
    - Test amb el moviment d'un objecte

---
+++ nutella.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+func _ready():
+	pass # Replace with function body.
+	
+func _process(delta):
+	position += Vector2(3,0)
+	
